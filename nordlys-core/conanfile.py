878a6ce3e0783b6dbd826bf738a07e960b2edb14
Botir Khaltaev <btrghstk@gmail.com>
2026-01-11 08:13:34 +0000

    chore: bump nordlys-core to v0.1.6

--- nordlys-core/conanfile.py
+++ nordlys-core/conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import cmake_layout
 
 class NordlysCoreConan(ConanFile):
     name = "nordlys_core"
-    version = "0.1.5"
+    version = "0.1.6"
 
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
